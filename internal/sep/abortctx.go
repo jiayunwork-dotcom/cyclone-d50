8be7c1ea3d0c11321bef81916aae7f9ddec28e4f
“jiayun” <jiayun_work@163.com>
2026-08-26 15:26:50 +0800

    fix: CutDiameter 不再立刻 cancel context

--- internal/sep/abortctx.go
+++ internal/sep/abortctx.go
@@ -2,9 +2,16 @@ package sep
 
 import "context"
 
+func liveCutContext() (context.Context, context.CancelFunc) {
+	return context.WithCancel(context.Background())
+}
+
 func abortCutContext() error {
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
+	ctx, cancel := liveCutContext()
+	defer cancel()
+	if err := ctx.Err(); err != nil {
+		return err
+	}
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
